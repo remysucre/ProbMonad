94d224c76a4a062863ee7678480a98d94816a51b
Remy Wang <remywang@protonmail.com>
2016-10-09 21:55:02 -0400

    didn't muck things up

--- simple/another.hs
+++ simple/another.hs
@@ -6,58 +6,39 @@ type Prob = Rational
 type Dist a = a -> Prob
 type Face = Int
 
-type InfFace b = b -> Dist Face
-
-id :: InfFace Face
-id x = \y -> if y == x then 1 else 0
-
-small :: InfFace Bool
-small True = \f -> if f <= 3 then 1%3 else 0
-small False = \f -> if f > 3 then 1%3 else 0
+type Inf a b = b -> Dist a
 
 -- | infer da @ [prior dist of a] lb @ [likelyhood of b from a]
-infer :: Dist Face -> (Face -> Dist b) -> InfFace b
+infer :: Dist Face -> (Face -> Dist b) -> Inf Face b
 -- [F] -> [b] -> (F -> Prob) -> (F -> (b -> Prob)) -> (b -> (F-> Prob))
 infer da lb = \b a -> lb a b * da a
 
+-- | E1. infers a posterior distribution of faces after observing the face
+fid :: Inf Face Face
+fid x = \y -> if y == x then 1 else 0
+
+-- | E2. an example of an Inf type, infers a distribution of faces from an 
+-- observation of bool, where bool is whether the face <= 3
+small :: Inf Face Bool
+small True = \x -> if x <= 3 then 1%3 else 0
+small False = \x -> if x > 3 then 1%3 else 0
+
+-- posterior probability of throwing 3 from the observation that the throw is
+-- less than 3 (the predicate face <= 3 is True)
+p2Gt :: Prob
+p2Gt = small True 3
+
+-- | infers a posterior distribution of faces from the likely hood of marking
+-- the left (<= 3) column
+mark3 :: Inf Face Bool
+mark3 = infer d6 mark
+
+-- distribution of faces on a d6
 d6 :: Dist Face
 d6 n = if elem n [1..6] then 1%6 else 0
 
+-- likelyhood of marking the left col from each face
 mark :: Face -> Dist Bool
 mark a = \b -> if a <= 3 then (if b then 1 else 0)
                          else (if b then 0 else 1)
 
-mark3 :: InfFace Bool
-mark3 = infer d6 mark
-
--- 
--- small :: InfFace Bool
--- small True = Dist [ (Face 1, 1 % 3)
---                   , (Face 2, 1 % 3)
---                   , (Face 3, 1 % 3)
---                   , (Face 4, 0)
---                   , (Face 5, 0)
---                   , (Face 6, 0)
---                   ]
--- small False = Dist [ (Face 1, 0)
---                    , (Face 2, 0)
---                    , (Face 3, 0)
---                    , (Face 4, 1 % 3)
---                    , (Face 5, 1 % 3)
---                    , (Face 6, 1 % 3)
---                    ]
--- 
--- -- | infer df @ [a priori dist of face] lb @ [likelyhood of b given a]
--- infer :: Dist Face -> (Face -> Dist b) -> InfFace b
--- infer (Dist df) lb = map (\(f, p) -> lb f)
--- 
--- infer (Dist [(Face 1, 1%6),
---              (Face 2, 1%6), 
---              (Face 3, 1%6), 
---              (Face 4, 1%6),
---              (Face 5, 1%6),
---              (Face 6, 1%6)])
---       (\(Face f) -> if f <= 3 then Dist [(True, 1), (False, 0)] 
---                        else Dist [(True, 0), (False, 1)])
---       = small
--- 
